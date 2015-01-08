7424442c72b694d66bf8269e96c66a76c1799bb4
Brian Duggan <bduggan@usgcrp.gov>
2015-01-08 17:13:39 -0500

    gcis-indicators-viewer

--- grv.js
+++ grv.js
@@ -3,206 +3,76 @@
 var server;
 var report;
 
-server = 'http://data.globalchange.gov';
+server = 'http://data.gcis-dev-front.joss.ucar.edu';
 report = 'nca3';
 
-// globals
-var current_chapter;
-var current_category;
-var current_finding;
-var current_figure;
-var current_table;
-
-// jq.js
 $(main);
 
-function load_all_buttons(url, div, label, template) {
-    $.getJSON(url,
-        function(data) {
-            var t = $('<div id="' + div + '"><h3>' + label + '</h3>');
-            var html = $("#" + template).html();
-            $(data).each(function() { t.append(_.template(html, this)); });
-            t.append('</div>');
-            $('#main').append(t);
-        });
-}
-
-function load_chapters(report) {
-    load_all_buttons(server + '/report/' + report + '/chapter.json?all=1', 'chapter', 'Chapters', 'chapter_button' );
-}
-
-function clear_all() {
-    $('#shown_finding').remove();
-    $('#shown_figure').remove();
-    $('#shown_table').remove();
-    $('#findings').remove();
-    $('#figures').remove();
-    $('#tables').remove();
-}
-
-function load_findings(e) {
-    if (current_category) {
-        $('#category_' + current_category).removeClass('active');
-    }
-    current_category = 'findings';
-    $('#category_findings').addClass('active');
-    clear_all();
-    load_all_buttons(server + '/report/' + report + '/chapter/' + current_chapter + '/finding.json?all=1', 'findings', 'Findings', 'finding_button' );
-}
-
-function load_figures(e) {
-    console.log('load figures');
-    if (current_category) {
-        $('#category_' + current_category).removeClass('active');
-    }
-    current_category = 'figures';
-    $('#category_figures').addClass('active');
-    clear_all();
-    load_all_buttons(server + '/report/' + report + '/chapter/' + current_chapter + '/figure.json?all=1', 'figures', 'Figures', 'figure_button' );
-}
-
-function load_tables(e) {
-    if (current_category) {
-        $('#category_' + current_category).removeClass('active');
-    }
-    current_category = 'tables';
-    $('#category_tables').addClass('active');
-    clear_all();
-    load_all_buttons(server + '/report/' + report + '/chapter/' + current_chapter + '/table.json?all=1', 'tables', 'Tables', 'table_button' );
-}
-
-function choose_chapter(e) {
-    var t = $(e.target);
-    if (current_chapter) {
-        $('#chapter_' + current_chapter).removeClass('active');
-    }
-    current_chapter = t.attr('chapter_identifier');
-    t.addClass('active');
-    $('#chapter_contents_list').remove();
-    clear_all();
-    if (current_chapter) {
-        $.getJSON( server + '/report/' + report + '/chapter/' + current_chapter + '.json' , function(d) {
-            $('#main').append(_.template($('#chapter_contents').html(), d ));
-        } );
-    }
-}
-
-function choose_finding (e) {
-    var t = $(e.target);
-    var identifier = t.attr('finding_identifier');
-    if (current_finding) {
-        $('#finding_' + current_finding).removeClass('active');
-    }
-    current_finding = identifier;
-    $('#finding_' + identifier).addClass('active');
-    $('#shown_finding').remove();
-    $('#shown_figure').remove();
-    console.log('choose finding ' + identifier);
-    $.getJSON(server + '/report/' + report + '/chapter/' + current_chapter + '/finding/' + identifier + '.json',
-            function(d) {
-                $('#main').append(_.template($('#show_finding').html(),d));
-            });
-}
-
-function choose_table (e) {
-    var t = $(e.target);
-    var identifier = t.attr('table_identifier');
-    if (current_table) {
-        $('#table_' + current_table).removeClass('active');
-    }
-    current_table = identifier;
-    $('#table_' + identifier).addClass('active');
-    $('#shown_table').remove();
-    $('#shown_figure').remove();
-    console.log('choose table ' + identifier);
-    $.getJSON(server + '/report/' + report + '/chapter/' + current_chapter + '/table/' + identifier + '.json',
-            function(d) {
-                $('#main').append(_.template($('#show_table').html(),d));
-            });
-}
-
-function choose_figure (e) {
-    var t = $(e.target);
-    var identifier = t.attr('figure_identifier');
-    if (current_figure) {
-        $('#figure_' + current_figure).removeClass('active');
-    }
-    current_figure = identifier;
-    $('#figure_' + identifier).addClass('active');
-    $('#shown_finding').remove();
-    $('#shown_figure').remove();
-    console.log('choose figure ' + identifier);
-    $.getJSON(server + '/report/' + report + '/chapter/' + current_chapter + '/figure/' + identifier + '.json',
-            function(d) {
-                console.log('got ', d);
-                if (!d.images) {
-                    d.images = [];
-                }
-                if (!d.files) {
-                    d.files = [];
-                }
-                $('#main').append(_.template($('#show_figure').html(),d));
-                $('.image_identifier').each(
-                    function() {
-                        var div = $(this);
-                        var img = $("<img>");
-                        img.attr({class : "img-responsive"});
-                        var identifier = div.attr('image_identifier');
-                        console.log('getting image ' + identifier);
-                        $.getJSON( server + '/image/' + identifier + '.json', function(d) {
-                                var f = d.files;
-                                console.log('image has file : ',f);
-                                img.attr({src : server + f[0].url});
-                            });
-                        div.html(img);
-                    });
-            });
-}
-
-function set_report(r) {
-    $('#report_title').html(r.title);
-    $('#report_identifier').html(r.identifier);
-    $('#input_report_identifier').val(r.identifier);
-}
-function set_server(r) {
-    var l = $("<a>");
-    l.html(r.replace('http://',''));
-    l.attr({href : r, target : '_blank' });
-    $('#server').html(l);
-    $('#input_server').val(r);
-}
-
 function main() {
-    $.getJSON( server + '/report/' + report + '.json', function(d) {
-        $('#report_' + report).addClass('active');
-        set_report(d);
-        set_server(server);
-        load_chapters(report);
+    $('<a />',{ href : server, text : server} ).appendTo('#server');
+    $.getJSON( server + '/report.json?report_type=indicator&all=1', function(d) {
+        $(d).each(function(i,e) {
+            var li = $('<a />', {
+                text : e.title,
+                class : 'list-group-item indicator',
+                id :  e.identifier,
+                click : show_indicator,
+                href : e.href } );
+            li.appendTo('#toplist');
+        });
     });
-    $(document).delegate(".chapter_link","click",choose_chapter);
-    $(document).delegate(".findings","click",load_findings);
-    $(document).delegate(".figures","click",load_figures);
-    $(document).delegate(".tables","click",load_tables);
-    $(document).delegate(".finding_link","click",choose_finding);
-    $(document).delegate(".table_link","click",choose_table);
-    $(document).delegate(".figure_link","click",choose_figure);
 }
 
-function update_settings() {
-    server = $('#input_server').val();
-    report = $('#input_report_identifier').val();
-    clear_all();
-    current_chapter = null;
-    current_category = null;
-    current_finding = null;
-    current_figure = null;
-    current_table = null;
-    $('#chapter').remove();
-    $('#chapter_contents_list').remove();
-    $.getJSON( server + '/report/' + report + '.json', function(d) {
-        $('#report_' + report).addClass('active');
-        set_report(d);
-        set_server(server);
-        load_chapters(report);
+var shown;
+function show_indicator() {
+    var href = $(this).attr('href');
+    var id = $(this).attr('id');
+    if (shown) {
+        $('#' + shown ).toggleClass('active');
+    }
+    console.log('showing indicator', id);
+    $('#' + id ).toggleClass('active');
+    shown = id;
+    $.get( href, function(d) {
+        console.log('hi');
+        $('#myModalLabel').html(d.title);
+        $('#summary').css({'white-space':'pre-line'});
+        $('#summary').html(d.summary);
+        $('#files').html("");
+        $('#figures').html("");
+        $('#images').html("");
+        $('#data').html("");
+        $('#activities').html("");
+        $(d.files).each(function(i,e) {
+            var a = $('<a />', { href : e.href } );
+            var div = $('<div />', { class : 'thumbnail'} );
+            var img = $('<img />', { src : server + '/assets/' + e.thumbnail } );
+            a.html(img);
+            div.html(a);
+            $('#files').append(div);
+        });
+        $(d.report_figures).each(function(i,e) {
+            $.get(e.href, function(figure) {
+                console.log('figure ',figure);
+                $('#figures').html('<pre>' + JSON.stringify(figure,undefined,"  ") + "</pre>");
+                $(figure.images).each(function(i,e) {
+                    $('#images').html('<pre>' + JSON.stringify(e,undefined," ") + '</pre>' );
+                    $.get( server + '/image/' + e.identifier + '.json' , function(img) {
+                        $('#images').append("<pre>" + JSON.stringify(img.parents,"","  ") + "</pre>");
+                        $(img.parents).each(function(i,e) {
+                            $.get( server + e.url + '.json', function (dataset) {
+                                $('#data').append('<pre>' + JSON.stringify(dataset,"","  ") + "</pre>");
+                            } );
+                            $.get( server + e.activity_uri + '.json', function (activity) {
+                                $('#activities').append('<pre>' + JSON.stringify(activity,"","  ") + "</pre>");
+                            } );
+                        });
+                    } );
+                } );
+            });
+        });
+        $('#myModal').modal();
     });
-}
+    return false;
+};
+
